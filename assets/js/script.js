e1fbd96e95fe842b0b2467db99633c9356545bf0
rileyale001 <alexjtriley@gmail.com>
2021-05-12 08:51:59 -0700

    commiting on main

---
+++ assets/js/script.js
@@ -0,0 +1,103 @@
+var questionlist = [
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:0
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:1
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:3
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:3
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:3
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:1
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:1
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:2
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:2
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:3
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:2
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:1
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:0
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:2
+
+  },
+  {
+    question: "",
+    answer:["", "", "", ""],
+    correctAns:0
+
+  },
+]
+
+var begingingEl = document.getElementById("beginPage");
+var questionCardEl = document.getElementById("questionCard");
+var questionsEl = document.getElementById("question");
+var answersEl = document.getElementById("answerCard");
+var timerEl = document.getElementById("timer");
+
+var beginBtn = document.getElementById("beginBtn");
+var signBtn = document.querySelector("#sign");
+var returnBtn = document.querySelector("#return")
+ 
